5ce1904454b50ec7ab7bddf980575ab68657bd8b
TranPhucHieu02 <phuchieu20.01@gmail.com>
2023-05-23 22:56:38 +0700

    add-book

--- src/main/java/com/example/buoi5/Controller/ProductController.java
+++ src/main/java/com/example/buoi5/Controller/ProductController.java
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,22 +16,24 @@ import jakarta.validation.Valid;
 @Controller
 @RequestMapping("/products")
 public class ProductController {
-   @Autowired
-   private ProductService productService;
-   @GetMapping("/create")
-   public String create (Model model){
-       model.addAttribute("product", new Product());
-       return "create";
-   }
-   @PostMapping("/create")
-   public String create(@Valid Product newProduct,BindingResult bindingResult,Model model){
-    if (bindingResult.hasErrors()) {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/create")
+    public String create(Model model) {
         model.addAttribute("product", new Product());
-        model.addAttribute("title", "Add Product");
         return "create";
     }
-    productService.add(newProduct);
-       return "redirect:/home";
-   }
-   
+
+    @PostMapping("/create")
+    public String create(@Valid Product newProduct, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("product", new Product());
+            model.addAttribute("title", "Add Product123");
+            return "create";
+        }
+        productService.add(newProduct);
+        return "redirect:/home";
+    }
+
 }
