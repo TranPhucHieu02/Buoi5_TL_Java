714fa103f99b8c814035b2dfe21bc47dca26112e
TranPhucHieu02 <phuchieu20.01@gmail.com>
2023-05-23 11:21:47 +0700

    ntbuoi5

--- src/main/java/com/example/buoi5/Controller/ProductController.java
+++ src/main/java/com/example/buoi5/Controller/ProductController.java
@@ -3,15 +3,20 @@ package com.example.buoi5.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.buoi5.Model.Product;
 import com.example.buoi5.Services.ProductService;
 
+import jakarta.validation.Valid;
+
 @Controller
-@RequestMapping("/product")
+@RequestMapping("/products")
 public class ProductController {
    @Autowired
    private ProductService productService;
@@ -21,8 +26,14 @@ public class ProductController {
        return "create";
    }
    @PostMapping("/create")
-   public String create( Product newProduct,Model model){
+   public String create(@Valid Product newProduct,BindingResult bindingResult,Model model){
+    if (bindingResult.hasErrors()) {
+        model.addAttribute("product", new Product());
+        model.addAttribute("title", "Add Product");
+        return "create";
+    }
     productService.add(newProduct);
        return "redirect:/home";
    }
+   
 }
--- src/main/java/com/example/buoi5/Model/Product.java
+++ src/main/java/com/example/buoi5/Model/Product.java
@@ -1,10 +1,26 @@
 package com.example.buoi5.Model;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class Product {
     private int id;
+
+    @NotBlank(message = "Tên sản phẩm không được để trống.")
     private String name;
+
+    @Length(min = 0, max =500 , message = "Tên ảnh không được quá 50 ky tu")
     private String image;
+
+    @NotNull(message="Gía sản phẩm không được để trống")
+    @Min(value=1, message=" Gía không nhỏ hơn 1")
+    @Max(value=999999, message=" Gía không quá 99999")
     private long price;
+    
     public int getId() {
         return id;
     }
